8bdb3f87d35547e1dfaa0dcde125ba7845c0b0b0
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 22:10:42 -0700

    init

---
+++ OR-Logic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OR_Logic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine(GetX() | GetY());
+            System.Console.WriteLine(GetX() || GetY());
+            Console.ReadLine();
+        }
+
+        public static bool GetX()
+        {
+            System.Console.WriteLine("GetX()");
+            return true;
+        }
+
+        public static bool GetY()
+        {
+            System.Console.WriteLine("GetY()");
+            return false;
+        }
+    }
+}
